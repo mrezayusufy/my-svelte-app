30a25b151784420c2d44b1a5ada9bb178ef4aa96
M.Reza Yusufy <mrezayusufy@gmail.com>
2026-09-06 15:02:54 +0330

    init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+	documentation
+</p>
+<img
+	src="{base}/blog-img1.jpg"
+	alt="Café Giovanni's Kaffeewelt"
+	class="w-full max-w-2xl h-auto rounded-lg shadow-md my-4"
+/>
